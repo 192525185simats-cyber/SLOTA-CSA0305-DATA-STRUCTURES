23445b6a040196d1830883bfd36c10a47cbafe5b
Shaik roshni <192525185.simats@saveetha.com>
2026-08-28 14:08:12 +0530

    Add files via upload

---
+++ binary search/BINARY SEARCH.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int a[20],n,key,low,high,mid,i;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+
+    scanf("%d",&key);
+
+    low=0;
+    high=n-1;
+
+    while(low<=high) {
+        mid=(low+high)/2;
+
+        if(a[mid]==key) {
+            printf("Element found at position %d",mid);
+            return 0;
+        }
+
+        if(a[mid]<key)
+            low=mid+1;
+        else
+            high=mid-1;
+    }
+
+    printf("Element not found");
+
+    return 0;
+}
